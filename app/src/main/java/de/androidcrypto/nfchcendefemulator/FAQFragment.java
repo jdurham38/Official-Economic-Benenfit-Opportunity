9109b376410a71c03dfd3314f4cc866d48818102
Joshua Durham <jdurham9725@eagle.fgcu.edu>
2023-04-26 15:31:40 -0400

    Updated UI and FAQ

--- app/src/main/java/de/androidcrypto/nfchcendefemulator/FAQFragment.java
+++ app/src/main/java/de/androidcrypto/nfchcendefemulator/FAQFragment.java
@@ -53,16 +53,20 @@ public class FAQFragment extends Fragment {
 
         // Initialize the group titles and child messages
         mGroupTitles = new ArrayList<>();
-        mGroupTitles.add("How to add a card?");
-        mGroupTitles.add("FAQ2?");
-        mGroupTitles.add("FAQ3?");
+        mGroupTitles.add("How do you use the card in ‘Pay Now’?");
+        mGroupTitles.add("How do you add a card in “Add-a-Card”");
+        mGroupTitles.add("What is NFC");
+
         mChildMessages = new HashMap<>();
         List<String> group1Messages = new ArrayList<>();
-        group1Messages.add("Navigate to the 'Add Card' button on the navigation bar or home-screen, then enter a valid card number and expiration date from valid government benefit issuers (EBT, SNAP, ETC).");
-        List<String> group2Messages = new ArrayList<>();
-        group2Messages.add("FAQ2 Description Text.");
+        group1Messages.add("To use the 'Pay Now' feature, follow these steps: First, go to the 'Pay Now' tab. " +
+                "Next, click on the 'Pay Now' button. Then, hold your NFC-compatible phone close to the terminal to initiate the transaction. After the transaction has been processed, your phone will make a noise to indicate that it's been completed.");        List<String> group2Messages = new ArrayList<>();
+        group2Messages.add("To add a card using the \"Add-a-Card\" feature, go to the \"Add a Card\" tab in the Hotbar below. Once you have selected this tab, you will be prompted to enter your card number and expiration date. You can find these numbers on the approved issued card that you wish to add to your account. " +
+                "Enter these digits into the corresponding text boxes and click the \"Add Card\" button to complete the process. Once the card has been successfully added, you should be able to use the ‘Pay Now’ function. Additionally, make sure that the device you are doing can use NFC capabilities. ");
         List<String> group3Messages = new ArrayList<>();
-        group3Messages.add("FAQ3 Description Text.");
+        group3Messages.add("Near Field Communication (NFC) is a wireless technology that enables devices to exchange data with each other when they are in close proximity, usually within a few centimeters. " +
+                "This Digital Wallet incorporates NFC technology to read and write data to compatible cards, such as credit cards or transportation passes. By simply holding the device close to the card, users can easily access the information stored on it or update it with new data, making the process fast and seamless.\n");
+
 
         mChildMessages.put(mGroupTitles.get(0), group1Messages);
         mChildMessages.put(mGroupTitles.get(1), group2Messages);
